34bb044ca5c77a8f26b141ba8da97514cb324cac
rochtenfeldt <rickard.hammarberg94@gmail.com>
2022-06-11 13:33:55 +0200

    Added script for movement

---
+++ Project Natit/PlayerBody.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+export (int) var speed = 200
+export (float) var rotationSpeed = 1.5
+var velocity = Vector2()
+var rotationDirection = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+
+func GetInput():
+	rotationDirection = 0
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x += -1
+		
+	if Input.is_action_pressed("ui_up"):
+		velocity.y +=1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y -=1
+		
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	GetInput()
+	rotation += rotationDirection * rotationSpeed * delta
+	velocity = move_and_slide(velocity)
+	
+
+		
+	
